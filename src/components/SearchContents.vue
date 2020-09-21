9e4289b6262a5a2ac5077b865cf9d9af9829faa5
moepyxxx <sjpy_xx_1007@docomo.ne.jp>
2020-09-21 18:14:11 +0900

    [ADD]: 部立・季節の検索機能追加

--- src/components/SearchContents.vue
+++ src/components/SearchContents.vue
@@ -29,19 +29,45 @@
               <input type="radio" id="end" v-model="checkKeywordMethod" value="end">
             </div>
           </div>
+          <p>和歌集から探す</p>
+          <div class="c-search__contents_word_sub" v-for="book in bookList" :key="`search-${book.id}`">
+            <div class="c-search__contents_word_sub_inner">
+              <label :for="book.identifykey">{{ book.name }}</label>
+              <input type="checkbox" :id="book.identifykey" v-model="checkBooks" :value="book.name">
+            </div>
+          </div>
+          <p>部立から探す</p>
           <div class="c-search__contents_word_sub">
-            <p>和歌集から探す</p>
-            <div class="c-search__contents_word_sub_inner">
-              <label for="万葉集">万葉集</label>
-              <input type="checkbox" id="万葉集" v-model="checkBooks" value="万葉集">
+            <div class="c-radio">
+              <label for="zouka">雑歌</label>
+              <input type="radio" id="zouka" value="雑歌" v-model="checkType">
             </div>
-            <div class="c-search__contents_word_sub_inner">
-              <label for="金槐和歌集">金槐和歌集</label>
-              <input type="checkbox" id="金槐和歌集" v-model="checkBooks" value="金槐和歌集">
+            <div class="c-radio">
+              <label for="soumonka">相聞歌</label>
+              <input type="radio" id="soumonka" value="相聞歌" v-model="checkType">
             </div>
-            <div class="c-search__contents_word_sub_inner">
-              <label for="山家集">山家集</label>
-              <input type="checkbox" id="山家集" v-model="checkBooks" value="山家集">
+            <div class="c-radio">
+              <label for="banka">挽歌</label>
+              <input type="radio" id="banka" value="挽歌" v-model="checkType">
+            </div>
+          </div>
+          <p>季節から探す</p>
+          <div class="c-search__contents_word_sub">
+            <div class="c-radio">
+              <label for="spring">春</label>
+              <input type="radio" id="spring" value="春" v-model="checkSeason">
+            </div>
+            <div class="c-radio">
+              <label for="summer">夏</label>
+              <input type="radio" id="summer" value="夏" v-model="checkSeason">
+            </div>
+            <div class="c-radio">
+              <label for="authmn">秋</label>
+              <input type="radio" id="authmn" value="秋" v-model="checkSeason">
+            </div>
+            <div class="c-radio">
+              <label for="winter">冬</label>
+              <input type="radio" id="winter" value="冬" v-model="checkSeason">
             </div>
           </div>
         </div>
@@ -58,14 +84,24 @@
             <tr>
               <th>和歌</th>
               <td>和歌（かな）</td>
+              <td>作者</td>
+              <td>作者キーワード</td>
               <td>収録歌集</td>
+              <td>部立</td>
+              <td>季節</td>
+              <td>枕詞</td>
             </tr>
           </thead>
           <tbody>
             <tr v-for="waka in filterWakaList" :key="`result-${waka.id}`">
-              <th>{{ waka.waka }}</th>
+              <th>{{ waka.main }}</th>
               <td>{{ waka.kana }}</td>
+              <td>{{ waka.author }}</td>
+              <td>{{ correctArrayToList(waka.authorKeyword) }}</td>
               <td>{{ waka.book }}</td>
+              <td>{{ waka.type }}</td>
+              <td>{{ waka.season }}</td>
+              <td>{{ correctArrayToList(waka.makura) }}</td>
             </tr>
           </tbody>
         </table>
@@ -80,12 +116,28 @@ export default {
   data() {
     return {
       checkBooks: [],
+      checkSeason: "",
+      checkType: "",
       keyword: "",
       checkKeywordMethod: "and",
-      wakaList: []
+      wakaList: [],
+      bookList: {},
     }
   },
   computed: {
+    correctArrayToList() {
+      return function (array) {
+        if (array.length > 0 && array[0] !== "") {
+          let str = "";
+          array.forEach((value) => {
+            str = str + value + '、';
+          });
+          return str;
+        } else {
+          return '';
+        }
+      }
+    },
     filterWakaList() {
 
       // すべてのデータを取得
@@ -112,14 +164,18 @@ export default {
         const waka = this.wakaList[i];
 
         // 歌集チェック
-        if (hitFlug) {
-          if (this.checkBooks.length !== 0) {
-            this.checkBooks.forEach((value) => {
-              if(waka.book.indexOf(value) == -1) {
-                hitFlug = false;
-              }
-            });
-          }
+        if (this.checkBooks.length && hitFlug) {
+          hitFlug = this.checkInOrSearchMethod(this.checkBooks, waka.book);
+        }
+
+        // 部立チェック
+        if (this.checkType && hitFlug) {
+          hitFlug = this.checkInOrSearchMethod(this.checkType, waka.type);
+        }
+
+        // 季節チェック
+        if (this.checkSeason && hitFlug) {
+          hitFlug = this.checkInOrSearchMethod(this.checkSeason, waka.season);
         }
 
         // キーワードチェック
@@ -128,7 +184,7 @@ export default {
           // and検索の場合
           if (this.checkKeywordMethod === "and") {
             searchKeyword.forEach((value) => {
-              if (waka.waka.indexOf(value) == -1 && waka.kana.indexOf(value) == -1) {
+              if (waka.main.indexOf(value) == -1 && waka.kana.indexOf(value) == -1) {
                 hitFlug = false;
               }
             });
@@ -137,7 +193,7 @@ export default {
           } else {
             let isAnyMatch = false;
             searchKeyword.forEach((value) => {
-              if(waka.waka.indexOf(value) !== -1 || waka.kana.indexOf(value) !== -1) {
+              if(waka.main.indexOf(value) !== -1 || waka.kana.indexOf(value) !== -1) {
                 isAnyMatch = true;
               }
             });
@@ -161,6 +217,27 @@ export default {
     }
   },
   methods: {
+    checkInOrSearchMethod(checkList, checkKey) {
+      if (!Array.isArray(checkList)) {
+        if(checkList.indexOf(checkKey) !== -1) {
+          return true;
+        } else {
+          return false;
+        }
+      } else if (checkList.length !== 0) {
+        let isAnyMatch = false;
+        checkList.forEach((value) => {
+          if(checkKey.indexOf(value) !== -1) {
+            isAnyMatch = true;
+          }
+        });
+        if (!isAnyMatch) {
+          return false;
+        } else {
+          return true;
+        }
+      }
+    },
     checkDuplicate(list, waka) {
       let addFlug = true;
       for (let i = 0; i < list.length; i++) {
@@ -186,7 +263,28 @@ export default {
           this.wakaList = wakaList;
         }
       });
-    }
+    },
+    showAllBookData() {
+      const database = firebase.database();
+      const book = "book";
+      database.ref(book).on('value', (data) => {
+        if (data) {
+          const bookList = data.val();
+          let list = [];
+          if(bookList != null) {
+            Object.keys(bookList).forEach((val) => {
+              bookList[val].id = val;
+              list.push(bookList[val]);
+            })
+          }
+          this.bookList = bookList;
+        }
+      });
+      console.log(this.bookList);
+    },
+  },
+  created() {
+    this.showAllBookData();
   }
 }
 </script>
