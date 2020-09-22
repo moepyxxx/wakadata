78ab6f0de7bb5a36010af5324c7671200c81de98
moepyxxx <sjpy_xx_1007@docomo.ne.jp>
2020-09-22 16:38:32 +0900

    [UPD]: 大事なデータを隠す

--- src/main.js
+++ src/main.js
@@ -9,27 +9,15 @@ import router from './router'
 Vue.use(ElementUI);
 Vue.config.productionTip = false
 
-// const firebaseConfig = {
-//   apiKey: process.env.VUE_APP_API_KEY,
-//   authDomain: process.env.VUE_APP_AUTH_DOMAIN,
-//   databaseURL: process.env.VUE_APP_DATABASE_URL,
-//   projectId: process.env.VUE_APP_PROJECT_ID,
-//   storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
-//   messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
-//   appId: process.env.VUE_APP_APP_ID
-// };
-// firebase.initializeApp(firebaseConfig);
-
-var firebaseConfig = {
-  apiKey: "AIzaSyD6zvmsQ9hyXcyqip6HT7WMCSSFJ8Xkjkw",
-  authDomain: "wakadata-bbca9.firebaseapp.com",
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
   databaseURL: "https://wakadata-bbca9.firebaseio.com",
-  projectId: "wakadata-bbca9",
-  storageBucket: "wakadata-bbca9.appspot.com",
-  messagingSenderId: "850442454120",
-  appId: "1:850442454120:web:75bb2db9a825858889a7dd"
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID
 };
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 new Vue({
